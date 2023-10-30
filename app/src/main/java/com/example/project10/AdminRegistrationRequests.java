9c31dd3311da12780c1ff43572f2c36228beed24
Omar Abdul <omaraabdul9@gmail.com>
2023-10-30 14:24:22 -0400

    accept and reject buttons are now functional. Requests to admin now display user info

--- app/src/main/java/com/example/project10/AdminRegistrationRequests.java
+++ app/src/main/java/com/example/project10/AdminRegistrationRequests.java
@@ -4,62 +4,82 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.auth.FirebaseAuth;
+import android.widget.Button;
+import android.view.View;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class AdminRegistrationRequests extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private ArrayList<String> userDetailsList;
-
+    private ArrayList<String> userIds; // List to store user IDs
     private Button backButton;
-
     private UserAdapter adapter;
-    private FirebaseFirestore pendingFirestore;
+    private FirebaseFirestore firestore;
 
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_registration_requests);
-        recyclerView = (RecyclerView) findViewById(R.id.userList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        userDetailsList = new ArrayList<>();
-        pendingFirestore = FirebaseFirestore.getInstance();
 
-        pendingFirestore.collection("pending users").addSnapshotListener(new EventListener<QuerySnapshot>() {
+        recyclerView = findViewById(R.id.userList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        userDetailsList = new ArrayList<>();
+        userIds = new ArrayList<>();
+
+        firestore = FirebaseFirestore.getInstance();
+
+        firestore.collection("pending users").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
-            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException error) {
+            public void onEvent(@Nullable QuerySnapshot documentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    // Handle the error
+                    return;
+                }
+
                 userDetailsList.clear();
-                adapter = new UserAdapter(getApplicationContext(), userDetailsList);
-                for(DocumentSnapshot snapshot : documentSnapshots){
-                    if(snapshot.exists()) {
+                userIds.clear();
+                for (DocumentSnapshot snapshot : documentSnapshots) {
+                    if (snapshot.exists()) {
+                        String userId = snapshot.getId();
                         String firstName = snapshot.getString("firstName");
                         String lastName = snapshot.getString("lastName");
+                        String fullName = firstName + " " + lastName;
+
                         String role = snapshot.getString("role");
-                        String details = firstName + " " + lastName + ", " + role;
-                        userDetailsList.add(details);
+                        String phone = snapshot.getString("phone");
+                        String email = snapshot.getString("email");
+                        String healthCardNumber = snapshot.getString("healthCardNumber");
+                        String employeeNumber = snapshot.getString("employeeNumber");
+                        String specialties = snapshot.getString("specialties");
+
+                        String details = " Role: " + role + " Email: " + email + " Phone: " + phone;
+                        if ("patient".equals(role)) {
+                            details += " Health Card: " + healthCardNumber;
+                        } else if ("doctor".equals(role)) {
+                            details += " Employee No: " + employeeNumber;
+                            details +=" Specialties: " + specialties;
+                        }
+
+                        userDetailsList.add(fullName + " |" + details);
+                        userIds.add(userId);
                     }
                 }
-                adapter.notifyDataSetChanged();
+
+                adapter = new UserAdapter(getApplicationContext(), userDetailsList, userIds);
                 recyclerView.setAdapter(adapter);
                 recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayoutManager.VERTICAL));
             }
@@ -74,7 +94,6 @@ public class AdminRegistrationRequests extends AppCompatActivity {
                 finish();
             }
         });
-
-
     }
 }
+
--- app/src/main/java/com/example/project10/UserAdapter.java
+++ app/src/main/java/com/example/project10/UserAdapter.java
@@ -1,102 +1,104 @@
 package com.example.project10;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
-    //User Adaptor
     private Context context;
-    private ArrayList<String> users;
+    private ArrayList<String> users; // List of user details
+    private ArrayList<String> userIds; // List of user IDs
+    private FirebaseFirestore firestore;
 
-    private FirebaseFirestore newFirestore;
-
-    public UserAdapter(Context context, ArrayList<String> users){
+    public UserAdapter(Context context, ArrayList<String> users, ArrayList<String> userIds) {
         this.context = context;
         this.users = users;
+        this.userIds = userIds;
+        this.firestore = FirebaseFirestore.getInstance(); // Initialize Firestore
     }
 
     @NonNull
     @Override
-    public UserAdapter.UserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-        View view = LayoutInflater.from(context).inflate(R.layout.user_layout_item,
-                parent, false);
+    public UserAdapter.UserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.user_layout_item, parent, false);
         return new UserHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull UserAdapter.UserHolder holder, int position){
+    public void onBindViewHolder(@NonNull UserAdapter.UserHolder holder, int position) {
         String user = users.get(position);
-        holder.setDetails(user);
+        String userId = userIds.get(position); // Get the corresponding userId
+        holder.setDetails(user, userId);
     }
+
     @Override
     public int getItemCount() {
         return users.size();
     }
 
-    //User Holder
-    class UserHolder extends RecyclerView.ViewHolder{
+    class UserHolder extends RecyclerView.ViewHolder {
         private TextView textName, textRole;
         private Button acceptButton, denyButton;
 
-        public UserHolder(@NonNull View itemView){
+        UserHolder(@NonNull View itemView) {
             super(itemView);
             textName = itemView.findViewById(R.id.textName);
             textRole = itemView.findViewById(R.id.textRole);
 
             acceptButton = itemView.findViewById(R.id.acceptButton);
             denyButton = itemView.findViewById(R.id.denyButton);
+        }
+
+        void setDetails(String user, String userId) {
+            String[] parts = user.split(" \\| ");
+            if (parts.length > 0) {
+                textName.setText(parts[0]);
+            }
+            if (parts.length > 1) {
+                textRole.setText(parts[1]);
+            }
+
 
             acceptButton.setOnClickListener(new View.OnClickListener() {
-                @Override
                 public void onClick(View v) {
-//                    newFirestore = FirebaseFirestore.getInstance();
-//
-//                    Map<String, Object> user = new HashMap<>();
-//                    user.put("name", textName);
-//                    newFirestore.collection("users").document().set(user);
+                    moveUserToCollection(userId, "accepted users");
                 }
             });
 
             denyButton.setOnClickListener(new View.OnClickListener() {
-                @Override
                 public void onClick(View v) {
-
+                    moveUserToCollection(userId, "rejected users");
                 }
             });
         }
+    }
 
+    private void moveUserToCollection(String userId, String collectionName) {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
-
-        void setDetails(String user){
-            String[] details = user.split(", ");
-            textName.setText(details[0]);
-            textRole.setText(details[1]);
-        }
+        firestore.collection("pending users").document(userId).get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    Map<String, Object> userData = documentSnapshot.getData();
+                    if (userData != null) {
+                        firestore.collection(collectionName).document(userId).set(userData)
+                                .addOnSuccessListener(aVoid ->
+                                        firestore.collection("pending users").document(userId).delete()
+                                );
+                    }
+                });
     }
 }
+
+
+
