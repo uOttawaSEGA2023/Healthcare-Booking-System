665e0a2b2ea09c83991b56d158b7c84223e4d178
AATaimah <129572284+AATaimah@users.noreply.github.com>
2023-11-15 21:31:48 -0500

    added some functionality to doctor upcoming appointment

--- app/src/main/java/com/example/project10/DoctorUpcomingAppointmentsActivity.java
+++ app/src/main/java/com/example/project10/DoctorUpcomingAppointmentsActivity.java
@@ -1,14 +1,71 @@
-
 package com.example.project10;
 
-        import android.os.Bundle;
-        import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TimePicker;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Date;
 public class DoctorUpcomingAppointmentsActivity extends AppCompatActivity {
 
+    private Button logOutButton, backButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore fstore;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_upcoming_appointments);
+
+
+        logOutButton = findViewById(R.id.doctorLogOut);
+        backButton = findViewById(R.id.backRequestButton);
+
+        mAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent mainIntent = new Intent(DoctorUpcomingAppointmentsActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(DoctorUpcomingAppointmentsActivity.this, DoctorWelcomeActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        });
     }
 }
